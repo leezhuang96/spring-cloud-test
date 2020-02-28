6154a428351cc13662096120030f9859e0f33a84
Jordon Li <leezhuang96@gmail.com>
2020-02-28 15:56:40 +0800

    Add home server provider.

---
+++ home-server-provider/src/main/java/com/leezhuang/HomeServerProviderApplication.java
@@ -0,0 +1,11 @@
+package com.leezhuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServerProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeServerProviderApplication.class, args);
+    }
+}
